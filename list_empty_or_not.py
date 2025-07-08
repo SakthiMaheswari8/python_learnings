3241f33d07d2c841dffe4d27acb6f9293bedcc92
SakthiMaheswari8 <shakthimaheswarip982@gmail.com>
2025-07-08 13:47:52 +0530

    list is empty or not

---
+++ list_empty_or_not.py
@@ -0,0 +1,11 @@
+"""
+This program contains wheather the given list is empty or not
+"""
+
+
+list1 = []
+if len(list1) == 0:
+    print("The list is Empty")
+    
+else:
+    print("The list is not empty")    
