570c73ff1222ceca136b00ece199dc064780c381
SakthiMaheswari8 <shakthimaheswarip9802@gmail.com>
2025-07-08 19:45:43 +0530

    merge two python dictionary

---
+++ merge_two_python_dict.py
@@ -0,0 +1,7 @@
+"""
+This program it to merge two python dictionary
+"""
+dicta={"name":"liya","age":5}
+dictb={"class":"UKG","section":"A"}
+dicta.update(dictb)
+print(dicta)
--- product.py
+++ product.py
@@ -1,6 +1,5 @@
 ''' This program is to implement the multiplication of values in the list'''
 
-
 b=[2,4,3,9,5]
 c=1
 for item in b:
