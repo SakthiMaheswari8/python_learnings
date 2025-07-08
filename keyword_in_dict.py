88ede3229f931c40c2f99674271f2bc574ae9240
SakthiMaheswari8 <shakthimaheswarip9802@gmail.com>
2025-07-08 14:53:47 +0530

    remove duplicate from list

--- keyword_in_dict.py
+++ keyword_in_dict.py
@@ -1,4 +1,4 @@
-'''this program is to find the keword already exist in the dictionary or not'''
+'''This program is to find the keword already exist in the dictionary or not'''
 keyword=input("enter the key word to find in list: ")
 dict_1={"name":"liya","age":5}
 if keyword in dict_1:
---
+++ remove_duplicate_from_list.py
@@ -0,0 +1,5 @@
+'''this is the program to remove duplicate from list'''
+num=[5,8,3,4,5,8]
+print("list before removing duplicates: ",num)
+n_um=set(num)
+print("list after removinfg duplicate:",list(n_um))
