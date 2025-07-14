b5a2f1511fa81c7f2fb9acadd476a96790511194
SakthiMaheswari8 <sakthimaheswarip9802@gmail.com>
2025-07-14 10:38:55 +0530

    Update simple_caculater.py

--- simple_caculater.py
+++ simple_caculater.py
@@ -1,5 +1,5 @@
 '''simple calculater '''
-def add(a,b):
+ def add(a,b):
     return a + b
 def sub(a,b):
     return a - b
@@ -9,24 +9,30 @@ def div(a,b):
     if b == 0:
         return "Error: Division by zero"
     return a / b
-print("select your option \n"
+while (True):
+    print("select your option \n"
       "1)Addition \n"
       "2)Subtract\n"
       "3)multiplication\n"
-      "4)division\n")     
-choice=input("enter your choice:")
-a=int(input("Enter the value for a:"))
-b=int(input("enter the value for b:"))
-if choice=="1":
-    print("Addition of number is:",add(a,b))
-elif choice=="2":
-    print("subtraction of number is:",sub(a,b))
-elif choice=="3":
-    print("multiplication of number is:",mul(a,b))
-elif choice=="4":
-    print("division of number is:",div(a,b))
-else:
-    print("invalid value")
+      "4)division\n"
+      "5)exit\n")  
+    choice=input("enter your choice:")
+    if choice=="5":
+        print("thank you")
+        break
+    if choice in("1","2","3","4"):
+        a=int(input("Enter the value for a:"))
+        b=int(input("enter the value for b:")) 
+    if choice=="1":
+        print("Addition of number is:",add(a,b))
+    elif choice=="2":
+        print("subtraction of number is:",sub(a,b))
+    elif choice=="3":
+        print("multiplication of number is:",mul(a,b))
+    elif choice=="4":
+        print("division of number is:",div(a,b))
+    else:
+        print("invalid value")
 
 
 
