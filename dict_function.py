f9f2c755ca6a88cb7407f2142107b8c2881687ef
SakthiMaheswari8 <sakthimaheswarip9802@gmail.com>
2025-07-20 22:24:02 +0530

    Update dict_function.py

--- dict_function.py
+++ dict_function.py
@@ -1,4 +1,5 @@
-''' '''
+'''This script defines a function that accepts any number of keyword arguments 
+and prints each key-value pair. '''
 def datas(**data):
     for i,j in data.items():
         print(f"{i}:{j}")
